8d52cda1f27912e816e372b6e32395b0198515ec
aliyaitrat-bit <aliyaitrat@gmail.com>
2026-09-02 16:42:34 +0500

    Add GitHub Actions workflow and update BaseTest

---
+++ tests/Jothidam.Tests/Jothidam.Tests/BaseTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+
+namespace Jothidam.Tests
+{
+    public class BaseTest : IDisposable
+    {
+        protected AndroidDriver Driver;
+
+        public BaseTest()
+        {
+            var options = new AppiumOptions();
+            options.PlatformName = "Android";
+            options.AutomationName = "UiAutomator2";
+            options.DeviceName = "emulator-5554";
+
+            string appPath = Environment.GetEnvironmentVariable("APPIUM_APP_PATH")
+                ?? @"C:\Users\PMLS\appiumrelated\app-prod-release.apk";
+
+            options.App = appPath;
+
+            // Appium v5 handles prefixes automatically for additional options
+            options.AddAdditionalAppiumOption("appPackage", "com.perurinc.virichiprovider");
+            options.AddAdditionalAppiumOption("appActivity", "com.perurinc.virichiprovider.MainActivity");
+
+            Driver = new AndroidDriver(new Uri("http://127.0.0.1:4723/"), options);
+        }
+
+        public void Dispose()
+        {
+            Driver?.Quit();
+        }
+    }
+}
---
+++ tests/Jothidam.Tests/Jothidam.Tests/FirstLaunchTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Jothidam.Tests
+{
+    public class FirstLaunchTest : BaseTest
+    {
+        [Fact]
+        public void AppLaunchesSuccessfully()
+        {
+            Assert.NotNull(Driver.SessionId);
+        }
+    }
+}
